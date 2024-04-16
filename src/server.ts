264b582f5300ef1e729958eb2c23486fbe93bedb
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2024-04-16 10:05:40 -0400

    chore: add eslint and configs

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  port: env.PORT,
-  host: "0.0.0.0",
-}).then(() => console.log("🚀 Server is running!"));
+app
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+  })
+  .then(() => console.log('🚀 Server is running!'))
