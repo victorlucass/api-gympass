e358173613df401a6739c0571080497b116d7f2c
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2024-04-27 19:52:59 -0400

    feat: create search gym for title

--- src/http/services/tests/search-gyms.test.ts
+++ src/http/services/tests/search-gyms.test.ts
@@ -32,5 +32,28 @@ describe('Fetch User Check-in History Service', () => {
     })
 
     expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'JavaScript Gym' })])
+  })
+
+  it('should be able to fetch paginated gyms search', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await repository.create({
+        id: `gym-${i}`,
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+        title: `JavaScript Gym ${i}`,
+      })
+    }
+
+    const { gyms } = await sut.execute({
+      query: 'JavaScript Gym',
+      page: 2,
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'JavaScript Gym 21' }),
+      expect.objectContaining({ title: 'JavaScript Gym 22' }),
+    ])
   })
 })
